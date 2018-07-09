eaaa19cdeb93e4a64979b0f4d67cb1612e357695
Apm5 <ljk13520367253@gmail.com>
2018-07-09 18:32:38 +0800

    Create tf_dataset_test.py
    To test the tf.data

---
+++ tf_dataset_test.py
@@ -0,0 +1,25 @@
+import tensorflow as tf
+
+X = tf.constant([1.3, 2.5, -3.1, 2.2, -0.6, -6.2])
+Y = tf.constant([1, 2, 3, 4, 5, 6])
+dataset = tf.data.Dataset.from_tensor_slices((X, Y))
+dataset = dataset.shuffle(buffer_size=10).batch(2).repeat(10)
+
+
+iterator = dataset.make_initializable_iterator()
+next_element = iterator.get_next()
+
+sess = tf.Session()
+
+init = tf.global_variables_initializer()
+sess.run(init)
+sess.run(iterator.initializer)
+
+# print(sess.run(next_element[0]))
+print(sess.run(next_element))
+print(sess.run(next_element))
+print(sess.run(next_element))
+print(sess.run(next_element))
+print(sess.run(next_element))
+print(sess.run(next_element))
+print(sess.run(next_element))
