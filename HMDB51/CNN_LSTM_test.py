80a11ac6f2783350af1b3503b4b6e3ab417ead2c
lijiakun <lijiakun@levelup.ai>
2018-07-20 14:27:39 +0800

    prestore img to numpy

--- HMDB51/CNN_LSTM_test.py
+++ HMDB51/CNN_LSTM_test.py
@@ -266,8 +266,12 @@ for epoch in range(epoch_num):
         print(i)
         data, label = sess.run(train_next_batch)
         if len(data) == batch_size:
+            t = time.time()
             data = load_prestored_data(data, depth, height, width)
+            print(time.time() - t)
+            t = time.time()
             num, _ = sess.run([correct_num, train_step], feed_dict={x: data, y_: label, BN_train: True, keep_prob: 0.5, learning_rate: lr})
+            print(time.time() - t)
             train_correct += num
     print('epoch:%d ' % epoch)
     print('train accuracy: %f ' % (train_correct / train_num))
